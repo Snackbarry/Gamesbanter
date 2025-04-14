fae4da3216580fb6a544723b4f2a6bd056e8634c
Snackbarry <barry4000@gmail.com>
2025-04-14 16:13:50 +0200

    Delete pong.js

--- pong.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById('pongCanvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = "white";
-ctx.fillRect(50, 50, 10, 50); // Paddle example
-ctx.fillRect(540, 50, 10, 50); // Paddle example
-ctx.beginPath();
-ctx.arc(300, 200, 8, 0, Math.PI*2);
-ctx.fill(); // Ball example
