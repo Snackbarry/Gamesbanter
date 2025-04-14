404b18dc7b2e3a45a5255fed44cd6dfbcbac23ef
Snackbarry <barry4000@gmail.com>
2025-04-14 18:22:03 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+// games.js
+const games = [
+    {
+      name: "Snake",
+      url: "snake.html",
+      thumb: "https://via.placeholder.com/300x180?text=Snake"
+    },
+    {
+      name: "Pong",
+      url: "pong.html",
+      thumb: "https://via.placeholder.com/300x180?text=Pong"
+    },
+    {
+      name: "Tetris",
+      url: "tetris.html",
+      thumb: "https://via.placeholder.com/300x180?text=Tetris"
+    }
+  ];
+  
+  // Zorg dat je dit bestand koppelt met <script src="games.js"></script> in index.html & games.html
+  
